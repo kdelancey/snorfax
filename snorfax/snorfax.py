806a3feb21d1092c28f61dd3eb3b3dd866e74378
kdelancey <pandaplox@gmail.com>
2016-12-29 20:38:23 -0500

    Fixed the code.

--- snorfax/snorfax.py
+++ snorfax/snorfax.py
@@ -10,21 +10,22 @@ class snorfax:
   async def snorfax(self):
   
   #URL, get object from whatever magic call happens
-    snorfax = None
+    snorfaxtxt = None
+    htmlObj = None
     url = "http://bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)" #build the web adress
     async with aiohttp.get(url) as response:
   #The HTML Object to act on and get text from.
       htmlObj = BeautifulSoup(await response.text(), "html.parser")
-  try:
-  #Snorfax
-    if soupObject.find_all('li') is not None:#ew, bad temp code
-      snorfax = soupObject.find_all('li')[0].get_text()
-  
-    if snorfax is not None: #if fax is not bad
-      await self.bot.say("Here is your Snorfax:\n\"" + snorfax + "\"")
       
-  except:
-    await self.bot.say("There's an error in Kyle's code.")
+    if ( htmlObj is not None ):
+      try:
+      #Snorfax
+        if htmlObj.find_all('li') is not None:#ew, bad temp code
+          snorfaxtxt = htmlObj.find_all('li')[0].get_text()
+        if snorfax is not None: #if fax is not bad
+          await self.bot.say("Here is your Snorfax:\n\"" + snorfaxtxt + "\"")
+      except ValueError:
+        await self.bot.say("There's an error in Kyle's code.")
 
 def setup(bot):
   bot.add_cog(snorfax(bot))
