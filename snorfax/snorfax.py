f050d3aa1835584d4c499c8ad5e23549e5a33e6f
kdelancey <pandaplox@gmail.com>
2016-12-29 21:05:30 -0500

    Fixed?

--- snorfax/snorfax.py
+++ snorfax/snorfax.py
@@ -18,7 +18,8 @@ class snorfax:
   #URL, get object from whatever magic call happens
     snorfaxtxt = None
     htmlObj = None
-    url = "https://en.wikipedia.org/wiki/Sochi" #build the web adress
+    paragraph = None
+    url = "https://en.wikipedia.org/wiki/Bulbasaur" #build the web adress
     async with aiohttp.get(url) as response:
   #The HTML Object to act on and get text from.
       htmlObj = BeautifulSoup(await response.text(), "html.parser")
@@ -26,8 +27,10 @@ class snorfax:
     if ( htmlObj is not None ):
       try:
       #Snorfax
-        if htmlObj.find_all('li') is not None:#ew, bad temp code
-          snorfaxtxt = htmlObj.find_all('li')[0].get_text()
+        paragraph = htmlObj.find_all('p')
+        if paragraph is not None:#ew, bad temp code
+          rdm = random.randint(0, len(paragraph))
+          snorfaxtxt = paragraph[rdm].get_text()
         if snorfax is not None: #if fax is not bad
           await self.bot.say("Here is your Snorfax:\n\"" + snorfaxtxt + "\"")
       except ValueError:
